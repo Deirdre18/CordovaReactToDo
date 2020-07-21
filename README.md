be83a8f3f2f5e50bfc733eb834ee1f4ecbed8152
Deirdre18 <deirdreweldon18@gmail.com>
2020-07-21 04:47:05 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# cordovaReactToDo
+
+## Following these tutorials to create responsive Reactjs app with cordova
+
+https://medium.com/@pshubham/using-react-with-cordova-f235de698cc3
+https://www.youtube.com/watch?v=sBws8MSXN7A
---
+++ platforms/android/app/src/main/assets/www/precache-manifest.dd1d876fd8476aa7b29de902c95fe951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1bb18efec95542b320188d0b7f1c149",
+    "url": "./index.html"
+  },
+  {
+    "revision": "099bcdd2b73cc17ca9d3",
+    "url": "./static/css/main.489eafc6.chunk.css"
+  },
+  {
+    "revision": "3de9c1099eb38b28f043",
+    "url": "./static/js/2.afa3f88b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.afa3f88b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "099bcdd2b73cc17ca9d3",
+    "url": "./static/js/main.251d6615.chunk.js"
+  },
+  {
+    "revision": "a19d6302fdfd1549f5fc",
+    "url": "./static/js/runtime-main.760c15e9.js"
+  }
+]);
